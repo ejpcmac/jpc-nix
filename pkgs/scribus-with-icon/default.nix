867b65ad14ef324c03e58730627f79cd7485c044
Jean-Philippe Cugnet <jean-philippe@cugnet.eu>
2019-01-05 17:19:22 +0100

    [scribus-with-icon] Make the desktop item here

--- pkgs/scribus-with-icon/default.nix
+++ pkgs/scribus-with-icon/default.nix
@@ -1,22 +1,31 @@
-{ stdenv, scribus, imagemagick }:
+{ stdenv, makeDesktopItem, scribus, imagemagick }:
 
-stdenv.mkDerivation rec {
-  name = "${scribus.name}-with-icon";
+let
   icon = ./icon.svg;
+
+  desktopItem = makeDesktopItem rec {
+      name = "scribus";
+      exec = "${name} %f";
+      icon = name;
+      desktopName = "Scribus";
+      mimeType = "application/vnd.scribus";
+      categories = "Qt;Graphics;Publishing;";
+  };
+in
+
+stdenv.mkDerivation {
+  name = "${scribus.name}-with-icon";
 
   buildInputs = [ scribus imagemagick ];
 
   buildCommand = ''
     mkdir -p $out/bin $out/share/applications
     ln -s ${scribus}/bin/scribus $out/bin
-    cp ${scribus}/share/applications/scribus.desktop $out/share/applications
+    cp ${desktopItem}/share/applications/scribus.desktop $out/share/applications
 
     for i in 16 24 48 64 96 128 256 512; do
       mkdir -p $out/share/icons/hicolor/''${i}x''${i}/apps
       convert -background none -resize ''${i}x''${i} ${icon} $out/share/icons/hicolor/''${i}x''${i}/apps/scribus.png
     done
-
-    substituteInPlace $out/share/applications/scribus.desktop \
-      --replace "#MimeType" "MimeType"
   '';
 }
